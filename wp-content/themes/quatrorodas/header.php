7b7ad5539978b50802acd8b919241e9923f9c73a
Luiz Carlos <luiz.manhani@gmail.com>
2016-02-14 08:00:38 -0200

    primeiro commit

---
+++ wp-content/themes/quatrorodas/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <!--[if lt IE 9]>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
